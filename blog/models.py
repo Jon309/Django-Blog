c17532e774428f6fd04b3fd8827ef20f84ecc7b1
Jon <jhensh1@students.towson.edu>
2019-04-09 14:56:01 -0400

    Implemented database and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Each class is going to be its own table
+
+
+class Post(models.Model):
+    # Field of our table is a character
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    datePosted = models.DateTimeField(default=timezone.now)
+    # If a user is deleted then we want to delete the post
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
